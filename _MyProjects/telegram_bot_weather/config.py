5bb39b217271ab6a8fefa4ba89b2bffe30508409
Grevetail <st.grevetail@yandex.ru>
2021-11-16 18:44:22 +0300

    add Matplotlib ex.

---
+++ _MyProjects/telegram_bot_weather/config.py
@@ -0,0 +1,2 @@
+open_weather = 'd54b51a3428af2fdf8f0fc192527f726'
+tg_bot = '2083030911:AAEUREqVlNiRizD6MTn8oIkO4jxNu0NqDwQ'
---
+++ _MyProjects/web_scraping_parsing_catalog/config.py
@@ -0,0 +1,2 @@
+URL = 'https://www.xcomspb.ru/catalog/kompyuternye_komplektyyuschie/videokarty/videokarta/'
+HOST = 'https://www.xcomspb.ru'
